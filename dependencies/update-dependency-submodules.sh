f4446c2c19773f5653e67ca62919748597691083
Michael Gogins <michael.gogins@gmail.com>
2020-04-30 09:54:38 -0400

    Removing unused submodules.

--- dependencies/update-dependency-submodules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-echo "Updating all submodules for csound-android..."
-git submodule update --init --recursive --remote
-git submodule update --recursive
-git submodule status --recursive
-sudo -k
-echo "Finished updating all submodules for csound-android."

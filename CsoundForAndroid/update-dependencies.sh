1c79546cab3feccaedc89d4156c46550c9f847e3
Michael Gogins <michael.gogins@gmail.com>
2020-04-30 09:26:39 -0400

    Updating submodule branches.

--- CsoundForAndroid/update-dependencies.sh
+++ CsoundForAndroid/update-dependencies.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Updating all source dependencies of Csound for Android..."
-cd ndk-libraries
-./update-dependency-submodules.sh
-./update-dependency-repositories.sh
+git submodule update --init --recursive
+git submodule update --recursive
+git submodule status --recursive
+git submodule foreach git pull
+git branch
+echo "Finished updating all submodules for CsoundForAndroid."
 ls -ll
 cd ..
 echo "Finished updating all source dependencies of Csound for Android."

06ca0ee3101dc720d661c788bf9b123f9c6dc98f
Michael Gogins <michael.gogins@gmail.com>
2020-04-30 09:28:08 -0400

    Removed redundant script.

--- CsoundForAndroid/ndk-libraries/update-dependency-submodules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-clear 
-echo "Updating all submodules for CsoundForAndroid..."
-git submodule update --init --recursive
-git submodule update --recursive
-git submodule status --recursive
-git submodule foreach git pull
-git branch
-echo "Finished updating all submodules for CsoundForAndroid."

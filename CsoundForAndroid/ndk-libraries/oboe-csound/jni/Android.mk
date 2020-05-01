70ae2dc7da7201277d719d926ad7b0ced98cf322
Michael Gogins <michael.gogins@gmail.com>
2020-05-01 19:39:21 -0400

    New release with new-ish build system.

--- CsoundForAndroid/ndk-libraries/oboe-csound/jni/Android.mk
+++ CsoundForAndroid/ndk-libraries/oboe-csound/jni/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 CSOUND_SRC_ROOT := $(NDK_MODULE_PATH)/csound
+OBOE_SRC_ROOT := $(NDK_MODULE_PATH)/oboe/src
 
 LOCAL_MODULE := oboe
 
@@ -13,25 +14,54 @@ LOCAL_LDFLAGS += -Wl,--export-dynamic
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -lOpenSLES
 
 LOCAL_SRC_FILES := \
-    $(NDK_MODULE_PATH)/oboe/src/aaudio/AAudioLoader.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/aaudio/AudioStreamAAudio.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/fifo/FifoBuffer.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/fifo/FifoControllerIndirect.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/fifo/FifoControllerBase.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/fifo/FifoController.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/common/AudioStream.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/common/AudioStreamBuilder.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/common/Utilities.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/common/LatencyTuner.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/common/Version.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/common/Trace.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/common/StabilizedCallback.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/opensles/AudioInputStreamOpenSLES.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/opensles/AudioOutputStreamOpenSLES.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/opensles/AudioStreamBuffered.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/opensles/AudioStreamOpenSLES.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/opensles/EngineOpenSLES.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/opensles/OpenSLESUtilities.cpp \
-    $(NDK_MODULE_PATH)/oboe/src/opensles/OutputMixerOpenSLES.cpp
+$(OBOE_SRC_ROOT)/opensles/AudioStreamOpenSLES.cpp \
+$(OBOE_SRC_ROOT)/opensles/AudioStreamBuffered.cpp \
+$(OBOE_SRC_ROOT)/opensles/OpenSLESUtilities.cpp \
+$(OBOE_SRC_ROOT)/opensles/EngineOpenSLES.cpp \
+$(OBOE_SRC_ROOT)/opensles/OutputMixerOpenSLES.cpp \
+$(OBOE_SRC_ROOT)/opensles/AudioInputStreamOpenSLES.cpp \
+$(OBOE_SRC_ROOT)/opensles/AudioOutputStreamOpenSLES.cpp \
+$(OBOE_SRC_ROOT)/aaudio/AAudioLoader.cpp \
+$(OBOE_SRC_ROOT)/aaudio/AudioStreamAAudio.cpp \
+$(OBOE_SRC_ROOT)/common/SourceI16Caller.cpp \
+$(OBOE_SRC_ROOT)/common/LatencyTuner.cpp \
+$(OBOE_SRC_ROOT)/common/QuirksManager.cpp \
+$(OBOE_SRC_ROOT)/common/FilterAudioStream.cpp \
+$(OBOE_SRC_ROOT)/common/SourceFloatCaller.cpp \
+$(OBOE_SRC_ROOT)/common/StabilizedCallback.cpp \
+$(OBOE_SRC_ROOT)/common/DataConversionFlowGraph.cpp \
+$(OBOE_SRC_ROOT)/common/FixedBlockWriter.cpp \
+$(OBOE_SRC_ROOT)/common/AudioStreamBuilder.cpp \
+$(OBOE_SRC_ROOT)/common/AudioStream.cpp \
+$(OBOE_SRC_ROOT)/common/Utilities.cpp \
+$(OBOE_SRC_ROOT)/common/Trace.cpp \
+$(OBOE_SRC_ROOT)/common/Version.cpp \
+$(OBOE_SRC_ROOT)/common/FixedBlockAdapter.cpp \
+$(OBOE_SRC_ROOT)/common/FixedBlockReader.cpp \
+$(OBOE_SRC_ROOT)/common/AudioSourceCaller.cpp \
+$(OBOE_SRC_ROOT)/fifo/FifoBuffer.cpp \
+$(OBOE_SRC_ROOT)/fifo/FifoControllerBase.cpp \
+$(OBOE_SRC_ROOT)/fifo/FifoControllerIndirect.cpp \
+$(OBOE_SRC_ROOT)/fifo/FifoController.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/ClipToRange.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/FlowGraphNode.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/SampleRateConverter.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/SourceFloat.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/SinkFloat.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/SourceI24.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/SinkI16.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/RampLinear.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/SinkI24.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/ManyToMultiConverter.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/SourceI16.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/MonoToMultiConverter.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/SincResampler.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/SincResamplerStereo.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/IntegerRatio.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/PolyphaseResampler.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/PolyphaseResamplerMono.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/MultiChannelResampler.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/LinearResampler.cpp \
+$(OBOE_SRC_ROOT)/flowgraph/resampler/PolyphaseResamplerStereo.cpp
 
 include $(BUILD_SHARED_LIBRARY)

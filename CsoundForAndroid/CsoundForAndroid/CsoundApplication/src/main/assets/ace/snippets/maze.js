3d80e17e48673a10a376a595d299b4f91dd236ef
gogins <michael.gogins@gmail.com>
2023-08-28 21:07:34 -0400

    Updated Ace Editor (#21).

--- CsoundForAndroid/CsoundForAndroid/CsoundApplication/src/main/assets/ace/snippets/maze.js
+++ CsoundForAndroid/CsoundForAndroid/CsoundApplication/src/main/assets/ace/snippets/maze.js
@@ -1,5 +1,13 @@
-define("ace/snippets/maze",["require","exports","module"],function(e,t,n){"use strict";t.snippetText="snippet >\ndescription assignment\nscope maze\n	-> ${1}= ${2}\n\nsnippet >\ndescription if\nscope maze\n	-> IF ${2:**} THEN %${3:L} ELSE %${4:R}\n",t.scope="maze"});                (function() {
-                    window.require(["ace/snippets/maze"], function(m) {
+ace.define("ace/snippets/maze.snippets",["require","exports","module"], function(require, exports, module){module.exports = "snippet >\ndescription assignment\nscope maze\n\t-> ${1}= ${2}\n\nsnippet >\ndescription if\nscope maze\n\t-> IF ${2:**} THEN %${3:L} ELSE %${4:R}\n";
+
+});
+
+ace.define("ace/snippets/maze",["require","exports","module","ace/snippets/maze.snippets"], function(require, exports, module){"use strict";
+exports.snippetText = require("./maze.snippets");
+exports.scope = "maze";
+
+});                (function() {
+                    ace.require(["ace/snippets/maze"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
                         }
